3ca1e4e6942b880930444db37fa1f7f5e0ac45f9
Azmat <azmat1816@gmail.com>
2019-01-16 22:25:05 -0500

    First commit

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -1,8 +1,6 @@
-public class HelloWorld {
+public class HelloWord {
     public static void main(String[] args) {
-
         System.out.println("Hello World");
-        System.out.println("Good Evening");
-
+        System.out.println("Every one");
     }
 }
